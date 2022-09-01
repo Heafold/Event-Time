1f914e54d1e7461e413cc6a287a07a1306584ba7
Heafold <mcorentin62@gmail.com>
2022-09-01 10:52:11 +0200

    event-page

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -6,11 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class EventController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/event', name: 'app_event')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
+        return $this->render('event/index.html.twig');
     }
 }
